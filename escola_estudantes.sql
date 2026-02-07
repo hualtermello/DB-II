7d7b6908f33c795df228c0debba9d7485f47544d
hualtermello <hualter.mello@escola.pr.gov.br>
2026-02-06 21:39:05 -0300

    Add files via upload

---
+++ escola_estudantes.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: escola
+-- ------------------------------------------------------
+-- Server version	8.0.43-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estudantes`
+--
+
+DROP TABLE IF EXISTS `estudantes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudantes` (
+  `id_estudante` int NOT NULL AUTO_INCREMENT,
+  `data_nascimento` date NOT NULL,
+  `zona` varchar(10) NOT NULL,
+  `veiculo_proprio` bit(1) NOT NULL,
+  `profissao` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_estudante`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudantes`
+--
+
+LOCK TABLES `estudantes` WRITE;
+/*!40000 ALTER TABLE `estudantes` DISABLE KEYS */;
+INSERT INTO `estudantes` VALUES (1,'2006-02-19','leste',_binary '\0','suporte'),(2,'2002-04-02','oeste',_binary '','estoquista'),(3,'1999-08-03','norte',_binary '','admin'),(4,'2005-01-21','oeste',_binary '\0','estudante'),(5,'1993-12-25','sul',_binary '','frentista'),(6,'2000-04-08','norte',_binary '\0','diretor');
+/*!40000 ALTER TABLE `estudantes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-06 21:37:27
