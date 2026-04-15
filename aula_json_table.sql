e789465baad72f9656a9c2e490270a683460a644
hualtermello <hualter.mello@escola.pr.gov.br>
2026-04-15 19:56:28 -0300

    Enhance SQL query to include additional columns and join

--- aula_json_table.sql
+++ aula_json_table.sql
@@ -4,21 +4,8 @@ create schema empresa;
 use empresa;
 
 
-select t1.parentesco, t1.sexo from tb_object_funcionario
-cross join
-json_table(
-	json_extract(json, "$.Dependentes"), "$[*]"
-    columns(PARENTESCO varchar(20) path "$.Parentesco",
-		sexo varchar(2) path "$.Sexo")
-) as t1;
-
-/*
-Crie uma tabela baseada no campo dependentes com uma coluna para sexo, parentesco, 
-data de nascimento e nome do dependente (tp_object_funcionario). Alem disso, 
-utilize o INNER JOIN para adicionar o nome do departamento (tb.departamento).
-*/
-/*
-select t1.parentesco, t1.sexo, t1.data_nascimento, t1.nome_dependente from tb_object_funcionario
+select t1.parentesco, t1.sexo, t1.data_nascimento, t1.nome_dependente, tdp.nome_departamento
+from tb_object_funcionario
 cross join
 json_table(
 	json_extract(json, "$.Dependentes"), "$[*]"
@@ -27,7 +14,7 @@ json_table(
         data_nascimento varchar(50) path "$.Data_Nascimento",
         nome_dependente varchar(100) path "$.Nome_Dependente")
 ) as t1
-inner join tb.departamento
-on ;*/
+inner join tb_departamento as tdp
+on tdp.NUMERO_DEPARTAMENTO = json_unquote(json -> "$.Numero_Departamento");
 
 select * from tb_departamento;
